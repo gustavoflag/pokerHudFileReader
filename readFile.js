2be2aa49e539e9fed49fd06191ac6f3de2dc23f1
gustavo.bandeira@iteris.com.br <gustavo.bandeira@iteris.com.br>
2020-04-09 17:20:05 -0300

    Ajustes leitor

--- readFile.js
+++ readFile.js
@@ -1,4 +1,4 @@
-const exphbs = require('express-handlebars');
+/*const exphbs = require('express-handlebars');
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 5600;
@@ -19,10 +19,10 @@ console.log(`Leitor iniciado, acesse o hud em http://localhost:${port}`);
 global.jogadoresMao = [
   {nome: 'jogGlobal1'},
   {nome: 'jogGlobal2'}
-];
+];*/
 
 var fs = require('fs');
-var ft = require('file-tail').startTailing('teste.txt');
+//var ft = require('file-tail').startTailing('teste.txt');
 const axios = require('axios');
 const configGeral = require('./config.js')
 
@@ -32,15 +32,54 @@ var token;
 
 var i = 0;
 
-ft.on('line', function(line) {
+/*ft.on('line', function(line) {
   readLine(line);
-});
+});*/
+
+linhasArquivo = [];
+maos = [];
 
 var mao = {
   idPokerstars: ""
 };
 
-function readLine(line){
+var readEachLineSync = require('read-each-line-sync');
+ 
+readEachLineSync('teste.txt', function(line) {
+  console.log(line);
+  readPokerLine(line);
+});
+
+postMaoRecursivo();
+
+//maos.forEach(maoEnvio => {
+//var j = 0;
+
+/*postMao(maoEnvio, (err, message) => {
+  if (err){
+    console.log('----ERRO AO ENVIAR MÃO', maoEnvio.idPokerstars, 'detalhes API:', err, '----');
+  } else {
+    console.log(`----Mão ${maoEnvio.idPokerstars} Enviada - msg API: ${message}----`);
+  }
+});*/
+  
+
+function postMaoRecursivo(){
+  var maoEnvio = maos[0];
+  postMao(maoEnvio, (err, message) => {
+    if (err){
+      console.log('----ERRO AO ENVIAR MÃO', maoEnvio.idPokerstars, 'detalhes API:', err, '----');
+    } else {
+      console.log(`----Mão ${maoEnvio.idPokerstars} Enviada - msg API: ${message}----`);
+    }
+    maos.splice(0, 1);
+    if (maos.length > 0){
+      postMaoRecursivo();
+    }
+  });
+}
+
+function readPokerLine(line){
   if (line.indexOf("Hand #") != -1){
     var pattern = /#\d*:/;
     var res = line.match(pattern);
@@ -56,13 +95,10 @@ function readLine(line){
     leitura = false;
   } else if (line.indexOf("*** SUMMARY ***") != -1){
     leitura = false;
-    postMao(mao, (err, message) => {
-      if (err){
-        console.log('----ERRO AO ENVIAR MÃO', mao.idPokerstars, 'detalhes API:', err, '----');
-      } else {
-        console.log(`----Mão ${mao.idPokerstars} Enviada - msg API: ${message}----`);
-      }
-    });
+    maos.push(mao);
+    mao = {
+      idPokerstars: ""
+    };
   } else if (line.indexOf("Dealt to ") != -1){
 
   } else if (line.indexOf("Uncalled bet ") != -1
