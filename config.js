c272554280e94bfea3132d91f68579270585d032
gustavo.bandeira@iteris.com.br <gustavo.bandeira@iteris.com.br>
2020-04-22 00:24:12 -0300

    Ajustes mão e css

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-//exports.urlAPI = 'https://poker-hud-api.herokuapp.com';
-exports.urlAPI = 'http://localhost:5500';
+exports.urlAPI = 'https://poker-hud-api.herokuapp.com';
+//exports.urlAPI = 'http://localhost:5500';
 exports.userAPI = 'tqsop';
 exports.passwordAPI = 'Tqsop2020!';
 exports.theme = 'dark';
